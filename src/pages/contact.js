3a7a188234c3a6ba8830e31fd0bb9dd68941f75f
Jake Powers <jpowers1324@gmail.com>
2024-06-03 16:05:31 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,40 @@
 import styles from '../styles/contact-styles.css'
+import { useState } from "react";
 
 const Contact = () => {
+    const [result, setResult] = useState("");
+
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "c003858d-f28d-4395-91b8-ac3692c21de7");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
+
     return (
     <>
     <content id="contact-page">
             <div id="main-content">
                 <h1>Contact Us!</h1>
-                    <form
-                    id="contact-form"
-                    action="https://api.web3forms.com/submit"
-                    method="POST"
-                    >
-                        <input type="hidden" name="access_key" value="c003858d-f28d-4395-91b8-ac3692c21de7" />
-            
+                    <form onSubmit={onSubmit}>
                         <p>
                             <label for="name">Name:</label>
                             <input
@@ -31,22 +53,10 @@ const Contact = () => {
                             <label for="message">Message:</label>
                             <textarea id="message" name="message" placeholder="Let us know what you think!" required></textarea>
                         </p>
-                            <input
-                            type="hidden"
-                            name="redirect"
-                            value="https://web3forms.com/success"
-                            />
-                            <input
-                            type="hidden"
-                            name="subject"
-                            value="Contact from Jaspers Website"
-                            />
-                            <input type="hidden" name="from_name" value="My Website" />
                         <p>
                             <button type="submit">Submit Form</button>
                         </p>
-            
-                        <p id="result"></p>
+                        <p>{result}</p>
                     </form>
             </div>
             
